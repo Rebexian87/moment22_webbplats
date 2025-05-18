1965f64fc1f661dad22e4288d9082f592ae85d59
Rebecca Smith <rebecca.smith@hotmail.se>
2025-05-18 18:01:23 +0200

    Uppdaterat index.html, add.html,  skapat about.html. lagt till funktion för att kunna lägga till arbetsliverfarenhet med hjälp av formulär

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,25 @@
 
-        
-        getData()
+      
+"use strict"  
+
+
+//let data= [];
+
+//let submEl=
+ 
+window.onload = init;
+function init () {
+         getData() //När sidan startar så anropas funktionen getData()
+        document.getElementById("submits").addEventListener("click", createWorkexperience);
+
+    
+
+
+   
+}
+     
+       
+     //   submEl.addEventListener("click", createWorkexperience())
 
 
         async function getData() {
@@ -8,8 +27,7 @@
             
             const data = await response.json();
             displayData(data)
-
-
+            
             console.log(data);
             
         }
@@ -22,7 +40,46 @@
 
               data.forEach(exp => {  //Loopar igenom kurser
                 expEl.innerHTML += `<li>${exp.companyname}, ${exp.jobtitle}, ${exp.location}</li> `}); } //Skapar innehåll till lista
+        
 
+        
+        
+
+        
+
+
+      async function createWorkexperience (companyname, jobtitle, location){
+            let companynameEl=document.getElementById("companyname")
+            let jobtitleEl=document.getElementById("jobtitle")
+            let locationEl=document.getElementById("location")
+
+            companyname=companynameEl.value
+            jobtitle=jobtitleEl.value
+            location=locationEl.value
+
+            let workexperience = {  
+            companyname: companyname,
+            jobtitle: jobtitle,
+            location:location
+            }
+
+            const response = await fetch ("http://127.0.0.1:3000/api/workexperiences", {
+                method: "POST",
+                headers: {
+                    "content-type": "Application/json"
+                },
+                body: JSON.stringify(workexperience)
+            })
+            const data= await response.json();
+            console.log(data);
+            
+        }
+
+
+
+
+
+        
 
                             
 
