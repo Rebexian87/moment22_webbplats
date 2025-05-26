a31916872b6a19fdec6ca41602139f0e5483ecbe
Rebecca Smith <rebecca.smith@hotmail.se>
2025-05-26 13:26:06 +0200

    gjorde om kod för att skapa lista och lade till funktion för att delete från lista. uppdaterade  css-fil

--- src/js/app.js
+++ src/js/app.js
@@ -11,18 +11,14 @@ window.onload = init;
 function init () {
          getData() //När sidan startar så anropas funktionen getData()
         document.getElementById("submits").addEventListener("click", createWorkexperience);
-
-    
+      
 
 
    
 }
      
        
-     //   submEl.addEventListener("click", createWorkexperience())
-
-
-        async function getData() {
+         async function getData() {
             const response = await fetch ("http://127.0.0.1:3000/api/workexperiences")
             
             const data = await response.json();
@@ -38,11 +34,55 @@ function init () {
             let expEl = document.getElementById ("erfarenhet");
                 expEl.innerHTML="";
 
-              data.forEach(exp => {  //Loopar igenom kurser
-                expEl.innerHTML += `<li>${exp.companyname}, ${exp.jobtitle}, ${exp.location}</li> `}); } //Skapar innehåll till lista
+              data.forEach(exp => {  //Loopar igenom erfarenheterna
+                let newElLi= document.createElement ("li")      //Skapar nytt element (li)
+
+              let newText=document.createTextNode (exp.companyname+ " " + exp.jobtitle + " " + exp.location ) //Skapar texten till det som visas i listan
+              newElLi.setAttribute('id', exp.id) //Skapar attributet id
+           
+              newElLi.appendChild(newText) //Lägger newText som "barn" till newElLi
+              expEl.appendChild(newElLi) // Lägger newElLi som "barn" till expEl
+
+            
+  newElLi.addEventListener ("click", deleteWorkexperience, false); //anropar funktionen delete ´Workexperience vid klick
+                
+                
+
+                }
         
+             
+        
+        ); 
 
         
+    }
+    
+                 async function deleteWorkexperience(e) {
+
+              //oneTextFromList=expEl.innerhtml
+              
+                
+               let id= e.target.id;
+
+               console.log(id);
+               
+
+
+                 
+ 
+     const response = await fetch(`http://127.0.0.1:3000/api/workexperiences/${id}`, 
+        
+        { method: "DELETE",
+          headers: {
+                    "content-type": "Application/json"
+                 },
+          });
+          const data= await response.json();
+             console.log(data);
+            
+            
+            
+        } 
         
 
         
@@ -75,13 +115,3 @@ function init () {
             
         }
 
-
-
-
-
-        
-
-                            
-
-
- 
